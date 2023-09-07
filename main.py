16d3236edd7fe598b048810710fe3be9c5446066
Mr.X <79870712+n30mrx@users.noreply.github.com>
2023-09-07 17:26:06 +0300

    added proper licensing

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+# Qr code telegram bot
+# Copyright (C) 2023  Mr.X
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import telebot
 import qrcode
 bot = telebot.TeleBot("TOKEN")
+print("""    <program>  Copyright (C) <year>  <name of author>
+    This program comes with ABSOLUTELY NO WARRANTY;
+    This is free software, and you are welcome to redistribute it
+    under certain conditions;""")
 
 def crqr(message):
     if len(message.text)>0:
--- usdtoiqd.py
+++ usdtoiqd.py
@@ -1,3 +1,19 @@
+# USD -> IQD telegram bot
+# Copyright (C) 2023  Mr.X
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+print("""    USD -> IQD bot  Copyright (C) 2023  Mr.X
+    This program comes with ABSOLUTELY NO WARRANTY;
+    This is free software, and you are welcome to redistribute it
+    under certain conditions;""")
 import telebot, requests, logging
 from telebot import types
 
