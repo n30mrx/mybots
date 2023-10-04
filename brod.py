3fedd9154fafe7ab5fd5733fbd16f1ecb999f6ab
Mr.X <79870712+n30mrx@users.noreply.github.com>
2023-10-05 02:55:29 +0300

    license+antiflood

--- brod.py
+++ brod.py
@@ -1,4 +1,18 @@
+license = """telegram bot brodcast script
+Copyright (C) 2023  Mr.X
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+print(license)
 from timeit import default_timer as timer
+from telebot.util import antiflood
 
 
 def Brod(bot,message,ids):
@@ -19,11 +33,16 @@ Progress: 0%"""
     for Id in ids:
         i = i+1
         try:
-            bot.copy_message(
-                chat_id=Id,
-                message_id=message.id,
-                from_chat_id=message.chat.id,
-            )
+            antiflood(
+                bot.copy_message,
+                Id,message.chat.id,
+                message.id
+                )
+            # bot.copy_message(
+            #     chat_id=Id,
+            #     message_id=message.id,
+            #     from_chat_id=message.chat.id,
+            # )
             
             T = T+1
         except Exception as e:
